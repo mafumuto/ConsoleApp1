5e66091f11376dd5e411a43a72bea67d3f248bbd
MAHMUT <MAHMUT@DESKTOP-U97PTI6>
2022-01-25 11:27:12 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            string projectName = "ACME";
+            Console.Write("View English output: \n\t\t  @ c:\Exercise\$"{projectName}"\data.txt");
+
+
+
+            
+
+        }
+    }
+}
